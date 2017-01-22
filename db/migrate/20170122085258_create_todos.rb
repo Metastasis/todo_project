dac56203440f7ad2528a630eec3577858c5d31c6
Metastasis <d1mq44@gmail.com>
2017-01-22 12:00:39 +0300

    Fix migration

--- db/migrate/20170122085258_create_todos.rb
+++ db/migrate/20170122085258_create_todos.rb
@@ -1,7 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
-      t.number :project_id
+      t.integer :project_id
       t.string :text
       t.boolean :isCompleted
 
